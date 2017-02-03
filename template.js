1b15e625e6c1b69580b670e4015613f76cc2674c
Kevin Lamping <klamping@gmail.com>
2017-02-03 15:24:10 -0600

    Create template.js

---
+++ template.js
@@ -0,0 +1,20 @@
+var url = 'https://www.usaa.com';
+var formEl = '.login';
+var usernameEl = '.jusername';
+var passwordEl = '.jpassword';
+
+var validUser = 'username'
+var validPass = 'hunter2'
+
+var loggedInUrl = 'https://www.usaa.com/logged-in';
+
+var loginError = '';
+var missingUserNameError = '';
+
+fetch('/template/login.page.js')
+  .then(function () {
+
+  });
+  
+  
+https://stuk.github.io/jszip/documentation/examples.html
